fc4d7224741152fc4c877cc2f9a1e29b962cccd6
Skaria Thomas <jitheapen@gmail.com>
2020-07-23 21:55:31 +0530

    Updates

---
+++ precache-manifest.74bd2f384e5608ad9bd15791445e96fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f4e8634e374cea083ff403c810938e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "cb00db89c2b55acb849c",
+    "url": "/react-app/static/css/main.2cd5fe79.chunk.css"
+  },
+  {
+    "revision": "75a32856cdf45e713a22",
+    "url": "/react-app/static/js/2.f9d46c92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.f9d46c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb00db89c2b55acb849c",
+    "url": "/react-app/static/js/main.83020e77.chunk.js"
+  },
+  {
+    "revision": "3264858a04ebf7d34906",
+    "url": "/react-app/static/js/runtime-main.75638cbc.js"
+  }
+]);
